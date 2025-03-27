1a50bd7ded4bed2758915db31a3c55ad4663151d
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2025-03-27 14:40:44 +0000

    Finished

--- Cool_Website/index.mjs
+++ Cool_Website/index.mjs
@@ -1,6 +1,15 @@
 import express from 'express'
+import sqlite3 from 'sqlite3';
+sqlite3.verbose();
 
 const app = express();
+
+const database1 = new sqlite3.Database('FILENAME_HERE.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the database.');
+});
 
 app.set('view engine', 'ejs');
 
@@ -8,6 +17,12 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
       res.render("landing_page");
+});
+
+app.get("/staff", (req, res) =>{
+	database1.all("SELECT * FROM staff", [], (error, results)=>{
+	res.render("staff", {results: results});
+});
 });
 
 app.listen(5000, () =>{ console.log("Server started")});
